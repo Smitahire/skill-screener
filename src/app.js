4dda148b68a4eecee0243031735cd5522c78834f
Smitahire <ahiresmit007@gmail.com>
2025-08-05 05:40:04 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+
+import  prisma from "./database/prisma";
+
+const app = express();
+
+app.use(json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(urlencoded({ extended: true }));
+
+// routes
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
